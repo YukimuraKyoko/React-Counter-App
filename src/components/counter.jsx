e25be9748d8ba7d492108b35d7e618577b7cc810
Tina <TinaNguyen290@yahoo.com>
2020-12-26 00:01:19 -0800

    added stylizing to the counter appearance and the button using react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    
+    state = {
+        count: 0,
+        //imageUrl: 'https://picsum.photos/200'
+    };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
+
     render() { 
         return (<React.Fragment>
-            <h1>Hello World</h1>
-            <button>Increment</button>
+            
+            <span style={this.styles } className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
  
