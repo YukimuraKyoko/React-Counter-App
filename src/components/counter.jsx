0ab50f3abf1a49a7c86f71e5f7ff94b6d2ea8197
Tina <TinaNguyen290@yahoo.com>
2020-12-26 00:55:45 -0800

    displayed an example list using array.map(), and also had to dynamically implement a key for each element otherwise console will log errors

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
     state = {
         count: 0,
         //imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     styles = {
@@ -17,6 +18,10 @@ class Counter extends Component {
             
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+            </ul>
+            
             </React.Fragment>
         );
     }
