1e5446ccd309201b70406abf2ffee818c39fec63
Tina <TinaNguyen290@yahoo.com>
2020-12-26 01:06:32 -0800

    learned about conditional rendering (read description)
    true && true = true
    true && "Hi" = "Hi"  //because for strings "" is false and "anyNumofChars" is true, and it'll always return the last condition which is "Hi"
    
    true && "Hi" && 1 //same rule apples; 0 is false and any other number is true, so this will return 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,38 +4,29 @@ class Counter extends Component {
     state = {
         count: 0,
         //imageUrl: 'https://picsum.photos/200'
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: []
     };
 
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
+    renderTags(){
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+        
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
     }
 
     render() { 
 
         return (<React.Fragment>
+
+            {this.state.tags.length === 0 && 'Please create a new tag!'}
+            {this.renderTags()}
+           
             
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-            </ul>
             
             </React.Fragment>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0 ? "warning" : "primary");
-        return classes;
-    }
-
-    formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+  
 }
  
 export default Counter;
