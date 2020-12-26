dfe69b3980c5cc288a79e003561ddce7ed095b4a
Tina <TinaNguyen290@yahoo.com>
2020-12-26 01:27:54 -0800

    Learned Event handlers and how to bind them (see description)
    for the button onClick, we are "not" calling the handleIncrement method but we're referencing it. When doing this we don't have access to it because it would need to be normally instantiated first like "obj A", but we want to avoid this. So we go back to what we learned previously and create a custom constructor and binding 'this' function so that it will return a new instance of itself when referenced.
    
    Or even better; to just convert the said function to an arrow function which already inherits 'this' and we'll have access to the entire Counter object.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,56 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
+    /*
+    constructor(){
+        super();
+        //using bind we can set the value of 'this', it will return
+        //a new instance of the handleIncrement function, 'this' always 
+        //referencing the current object
+        this.handleIncrement = this.handleIncrement.bind(this);
+    }
+    */
     state = {
         count: 0,
         //imageUrl: 'https://picsum.photos/200'
-        tags: []
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
-    renderTags(){
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-        
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
     }
 
+    //by changing handleIncrement() to an arrow function, we don't
+    //need to create a constructor and binding 'this'
+    handleIncrement = () => {
+        console.log('Increment Clicked', this);
+    }
     render() { 
 
         return (<React.Fragment>
-
-            {this.state.tags.length === 0 && 'Please create a new tag!'}
-            {this.renderTags()}
-           
             
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick= {this.handleIncrement}className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+            </ul>
             
             </React.Fragment>
         );
     }
 
-  
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0 ? "warning" : "primary");
+        return classes;
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
 }
  
 export default Counter;
