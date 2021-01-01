f1209b76640ecc184ebc5fda894a8c5d93c381e1
Tina <TinaNguyen290@yahoo.com>
2021-01-01 13:41:39 -0800

    Learned how to do object destructuring
    In navbar.jsx: Got rid of the Navbar class and instead used a NavBar const arrow function, the argument passed in was in {} curly braces to indicate that it's a destructure, meaning whatever is inside the {} already has props as a variable.
    
    So passing in {totalCounters} is the same as this.props.totalCounters
    
    I did the same in counters.jsx except we simply added a const destructure on the needed functions or wherever this.props was used in the code. So this gets rid of retyping 'this.props' to make the code cleaner.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,16 +4,18 @@ import Counter from './counter';
 class Counters extends Component {
     
     render() { 
+        const {onReset, counters, onDelete, onIncrement} = this.props;
+
         return ( <div>
             <button 
-            onClick={this.props.onReset}
+            onClick={onReset}
             className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.props.counters.map(counter => (
+            {counters.map(counter => (
                 //everything in the "Counter" tag is a "prop" except 'key'
             <Counter 
             key={counter.id} 
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
             >
             </Counter>
