faa594ccc5c8a10db078d89623d9aa278448651a
Tina <TinaNguyen290@yahoo.com>
2021-01-01 12:54:15 -0800

    Lifted the state from counters.jsx to App.js, also transformed App function to Class App, And Navbar added to display total number of counter objects with value higher than 0
    The goal here is to create a Navbar to display total number of active counters with their values higher than 0.
    
    To do this I got a simple Navbar component from Bootstrap navbar examples and created a navbar.jsx component to render that navbar. Then I passed in the totalCounters that was created in App.js, and to get that I used "props".
    
    In App.js the state(), handleReset(), handleIncrement(), and handleDelete(), from counters.jsx was copied over since we're moving the state() and its functions to a new parent(App.js).
    
    App.js now displays the NavBar, has the functionality for <Counters>, and a new totalCounters variable to display in the NavBar that displays the length of the counters[] array if the value is higher than 0.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,25 +2,18 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id: 1, value: 4},
-            { id: 2, value: 0},
-            { id: 3, value: 0},
-            { id: 4, value: 0},
-        ]
-     };
+    
     render() { 
         return ( <div>
             <button 
-            onClick={this.handleReset}
+            onClick={this.props.onReset}
             className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.state.counters.map(counter => (
+            {this.props.counters.map(counter => (
                 //everything in the "Counter" tag is a "prop" except 'key'
             <Counter 
             key={counter.id} 
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
             >
             </Counter>
@@ -28,36 +21,7 @@ class Counters extends Component {
         </div> );
     }
 
-    handleDelete = (counterId) =>{
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters});
-    }
-
-    handleReset = () =>{
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({counters});
-    }
-
-    handleIncrement = counter =>{
-        //counters = new Array[] of this.state.counters
-        const counters = [...this.state.counters];
-
-        //current counter increment button is "counter, so index is that button"
-        const index = counters.indexOf(counter);
-        console.log(counter);
-
-        //cloned current index's counter
-        counters[index] = { ...counter }
-
-        //increment it by 1
-        counters[index].value++;
-
-        //replace the main state's counter with our new counter.
-        this.setState({counters});
-    }
+    
 }
  
 export default Counters;
